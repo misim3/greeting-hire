228c99deb6ebc782eb346016ab58d80ccfa1ce65
misim3 <misim@student.42seoul.kr>
2023-08-12 23:48:15 +0900

    undone position

---
+++ src/main/java/com/misim/greetinghire/Position.java
@@ -0,0 +1,15 @@
+package com.misim.greetinghire;
+
+import java.util.Date;
+
+class Position {
+
+    private long id;
+    private String company;
+    private String position;
+    private String experience;
+
+    private String type;
+    private Date deadline;
+    private String purl;
+}

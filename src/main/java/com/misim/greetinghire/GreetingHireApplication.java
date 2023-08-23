6e5234ab8cdb3a35c9bf9ca319f862d85916976d
misim3 <misim@student.42seoul.kr>
2023-08-24 07:59:19 +0900

    Init Project

---
+++ src/main/java/com/misim/greetinghire/GreetingHireApplication.java
@@ -0,0 +1,13 @@
+package com.misim.greetinghire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingHireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misim/greetinghire/GreetingHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misim.greetinghire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

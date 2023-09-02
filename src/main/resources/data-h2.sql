6eb9ea997a557b41f36b969d4c1dff2fc2c54c38
misim3 <misim@student.42seoul.kr>
2023-09-02 22:40:20 +0900

    Implement: yml, sql

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,2 @@
+insert into position (company, name, experience, type, url, created_date, modified_date) values ('테스트1', '테스트1의 직무', '테스트1의 경력', '테스트1의 유형', '테스트1의 링크', now(), now());
+insert into position (company, name, experience, type, url, created_date, modified_date) values ('테스트2', '테스트2의 직무', '테스트2의 경력', '테스트2의 유형', '테스트2의 링크', now(), now());
